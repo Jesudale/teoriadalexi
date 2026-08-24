17db4cb2856f290ba59f786d1574a2dea4214fdf
Alexi <contactolocalizadorgps@gmail.com>
2026-08-24 07:52:04 -0700

    primer commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  supabaseUrl: 'https://mamntrbdujhedspqqzqw.supabase.co',
+  supabaseUrl: 'https://jcdufidbcmsejwuqwlaf.supabase.co',
   supabaseKey:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1hbW50cmJkdWpoZWRzcHFxenF3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjYwODA5ODcsImV4cCI6MTk4MTY1Njk4N30.P1EDegpGgzadiKbFqJM_T9h-Ah0K4SQPq2Q8OQJUPfY',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpjZHVmaWRiY21zZWp3dXF3bGFmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQzOTQ4MzMsImV4cCI6MjA3OTk3MDgzM30.yx9PtlgVXb4i1984nu10aHAT-3zBT6WRHu-Ok7hmk04',
 };
