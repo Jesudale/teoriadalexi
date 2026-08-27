9b2c0810fba80bb061b3456ea1a775da2079aad1
Alexi <contactolocalizadorgps@gmail.com>
2026-08-27 11:40:37 -0700

    mejoras vista

--- src/app/pages/formula/formula.page.ts
+++ src/app/pages/formula/formula.page.ts
@@ -23,7 +23,7 @@ export class FormulaPage implements OnInit, AfterViewInit {
   circulacionChart: any;
   distribucionChart: any;
 
-  params = {
+ params = {
     a: 4,
     l: 394,
     k: 50,
@@ -34,6 +34,37 @@ export class FormulaPage implements OnInit, AfterViewInit {
     e: 2
   };
 
+  // valores máximos iniciales (igual que los iniciales de params)
+  maxA = 4;
+  maxL = 394;
+  maxK = 50;
+  maxCp = 1;
+  maxA0 = 110.4;
+  maxE = 10;
+
+    updateMax(field: string) {
+    switch (field) {
+      case 'a': this.params.a = this.maxA; break;
+      case 'l': this.params.l = this.maxL; break;
+      case 'k': this.params.k = this.maxK; break;
+      case 'cp': this.params.cp = this.maxCp; break;
+      case 'a0': this.params.a0 = this.maxA0; break;
+      case 'e': this.params.e = this.maxE; break;
+      case 'w': this.params.omega = this.params.omega; break;
+    }
+    this.updateCharts()
+  }
+
+    updatePi() {
+    this.params.pi = 1 - this.params.omega;
+    this.updateMax('w')
+  }
+  
+  // Formateador para el pin
+  formatValue(value: number) {
+    return value.toFixed(1); // siempre muestra con una coma decimal
+  }
+  
 resultado: any;
 chart: any;
 
@@ -67,7 +98,7 @@ chart: any;
   calcular() {
 this.params.omega = parseFloat(this.params.omega.toFixed(1));
   this.params.pi = parseFloat((1 - this.params.omega).toFixed(1));
-  
+
     this.supabaseApi.calcularEquilibrio(this.params).subscribe({
       next: (res) => {
         console.log('Resultado:', res);
@@ -85,7 +116,7 @@ this.params.omega = parseFloat(this.params.omega.toFixed(1));
     this.produccionChart = new Chart(this.produccionRef.nativeElement.getContext('2d')!, {
       type: 'bar',
       data: {
-        labels: ['a', 'l', 'k'],
+        labels: ['A', 'L', 'K'],
         datasets: [{
           label: 'Producción',
           data: [this.params.a, this.params.l, this.params.k],
@@ -98,7 +129,7 @@ this.params.omega = parseFloat(this.params.omega.toFixed(1));
     this.circulacionChart = new Chart(this.circulacionRef.nativeElement.getContext('2d')!, {
       type: 'bar',
       data: {
-        labels: ['cp', 'a0', 'e'],
+        labels: ['Cp', 'A0', 'e'],
         datasets: [{
           label: 'Circulación',
           data: [this.params.cp, this.params.a0, this.params.e],
@@ -111,7 +142,7 @@ this.params.omega = parseFloat(this.params.omega.toFixed(1));
     this.distribucionChart = new Chart(this.distribucionRef.nativeElement.getContext('2d')!, {
       type: 'bar',
       data: {
-        labels: ['omega', 'pi'],
+        labels: ['Salarios', 'Beneficios'],
         datasets: [{
           label: 'Distribución del ingreso',
           data: [this.params.omega, this.params.pi],
